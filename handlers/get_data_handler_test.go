b96fd3ad355d85657fd12a0b1e1e7908bf18e154
Riya Dennis <riyacv@gmail.com>
2018-07-11 21:29:23 +0100

    Added tests for get error conditions

--- handlers/get_data_handler_test.go
+++ handlers/get_data_handler_test.go
@@ -8,18 +8,19 @@ import (
 	"strings"
 	"github.com/aes-encryption/middleware"
 	"github.com/aes-encryption/handlers"
+	"fmt"
 )
 
 var _ = Describe("GetDataHandler", func() {
 
-	BeforeEach(func() {
+	BeforeSuite(func() {
 		config, err := middleware.GetConfig("../config_test.yaml")
 		Expect(err).To(BeNil())
 		go handlers.Run(config)
 
 	})
 
-	Context("get bad request for a bad post", func() {
+	Context("get bad request for a post", func() {
 		It("Should give me a valid text", func() {
 			data := `{
 	"data": "functions useful for working with such data",
@@ -28,6 +29,40 @@ var _ = Describe("GetDataHandler", func() {
 			message := strings.NewReader(data)
 			resp, err := http.Post("http://localhost:8081/store", "application/json", message)
 			Expect(err).To(BeNil())
+			Expect(resp.StatusCode).To(Equal(http.StatusBadRequest))
+		})
+	})
+
+	Context("get status bad request no header get", func() {
+		It("Should give me a valid text", func() {
+			resp, err := http.Get("http://localhost:8081/get")
+			Expect(err).To(BeNil())
+			Expect(resp.StatusCode).To(Equal(http.StatusBadRequest))
+		})
+	})
+
+	Context("get status bad request no id get", func() {
+		It("Should give error", func() {
+			req, err := http.NewRequest("GET", "http://localhost:8081/get", nil)
+			req.Header.Add("key", "test key")
+			client := http.Client{}
+			resp, err := client.Do(req)
+			Expect(err).To(BeNil())
+			Expect(resp.StatusCode).To(Equal(http.StatusBadRequest))
+		})
+	})
+
+	Context("get status bad request for id and key that is not in db", func() {
+		It("Should give me error", func() {
+			var b []byte
+			req, err := http.NewRequest("GET", "http://localhost:8081/get", nil)
+			req.Header.Add("key", "test key")
+			req.Header.Add("id", "test id")
+			client := http.Client{}
+			resp, err := client.Do(req)
+			Expect(err).To(BeNil())
+			resp.Body.Read(b)
+			fmt.Printf("%#v", string(b))
 			Expect(resp.StatusCode).To(Equal(http.StatusBadRequest))
 		})
 	})
