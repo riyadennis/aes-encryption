512aa7e51eb8bb3a3be1f4055aa26d6d8fd80c18
Riya Dennis <{ID}+{username}@users.noreply.github.com>
2019-03-28 21:46:32 +0000

    added test to verify signature

--- internal/dsig/signature.go
+++ internal/dsig/signature.go
@@ -4,22 +4,24 @@ import (
 	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
-	"crypto/sha1"
 	"crypto/sha256"
-	"crypto/sha512"
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
-	"hash"
 	"strings"
 )
 
-func VerifySignature(key *rsa.PublicKey, toSign, signed []byte) error {
+// VerifySignature checkes signed data and the hash
+func VerifySignature(
+	key *rsa.PublicKey,
+	toSign, signed []byte,
+	hash crypto.Hash,
+) error {
 	h := sha256.New()
 	h.Write(toSign)
 
 	d := h.Sum(nil)
-	err := rsa.VerifyPKCS1v15(key, crypto.SHA256, d, signed)
+	err := rsa.VerifyPKCS1v15(key, hash, d, signed)
 	if err != nil {
 		return err
 	}
@@ -28,7 +30,7 @@ func VerifySignature(key *rsa.PublicKey, toSign, signed []byte) error {
 
 // CreateSignature will create a  digital signature for the data
 // with the private key provided
-func CreateSignature(data string, key []byte, algorithm string) (string, error) {
+func CreateSignature(data string, key []byte, hash crypto.Hash) (string, error) {
 	block, _ := pem.Decode(key)
 	if block == nil {
 		return "", fmt.Errorf("invalid private key")
@@ -40,36 +42,15 @@ func CreateSignature(data string, key []byte, algorithm string) (string, error) 
 	if err != nil {
 		return "", err
 	}
-	d, err := createHash([]byte(data), algorithm)
-	if err != nil {
-		return "", err
-	}
+	h := hash.New()
+	h.Write([]byte(data))
+	d := h.Sum(nil)
 
-	sig, err := rsa.SignPKCS1v15(rand.Reader, pk, crypto.SHA256, d)
+	sig, err := rsa.SignPKCS1v15(rand.Reader, pk, hash, d)
 	if err != nil {
 		return "", err
 	}
 	return string(sig), nil
-}
-
-func createHash(data []byte, algo string) ([]byte, error) {
-	var h hash.Hash
-	switch algo {
-	case "sha256":
-		h = sha256.New()
-		break
-	case "sha1":
-		h = sha1.New()
-		break
-	case "sha526":
-		h = sha512.New()
-		break
-	}
-	_, err := h.Write([]byte(data))
-	if err != nil {
-		return nil, err
-	}
-	return h.Sum(nil), nil
 }
 
 func clean(s string) string {
--- internal/dsig/signature_test.go
+++ internal/dsig/signature_test.go
@@ -1,7 +1,12 @@
 package dsig
 
 import (
+	"crypto"
+	"crypto/rsa"
+	"crypto/x509"
 	"encoding/base64"
+	"encoding/pem"
+	"fmt"
 	"io/ioutil"
 	"testing"
 )
@@ -20,7 +25,7 @@ func TestCreateSignatureWithHashSha256(t *testing.T) {
 	if err != nil {
 		t.Errorf("failed :: %v", err)
 	}
-	sig, err := CreateSignature(messageToSign, key, "sha256")
+	sig, err := CreateSignature(messageToSign, key, crypto.SHA256)
 	if err != nil {
 		t.Errorf("failed :: %v", err)
 	}
@@ -31,50 +36,61 @@ func TestCreateSignatureWithHashSha256(t *testing.T) {
 }
 
 func TestCreateSignatureWithHashSha1(t *testing.T) {
+	expectedSha1 := `
+OSH1HartHf48R88LPod8BkB1atpJJrIAwrim0iO3Og+x
+QIiCI3xazErcRZjOrZnJRTUfRYPcyXMPx46f+CFD2O3P
+zyShTjwvCh5QwfRcOuvRmvlA5MfIG0nKxpBG71YSAujr
+7WHmzk8KyhQ5nHuMsDJ84l3PjQA6BuvzVXTOE5o= `
+
 	key, err := ioutil.ReadFile("server.key")
 	if err != nil {
 		t.Errorf("failed :: %v", err)
 	}
-	sig, err := CreateSignature(messageToSign, key, "sha526")
+	sig, err := CreateSignature(messageToSign, key, crypto.SHA1)
 	if err != nil {
 		t.Errorf("failed :: %v", err)
 	}
 	encodedSv := base64.StdEncoding.EncodeToString([]byte(sig))
-	if encodedSv != clean(expectedSha256) {
+	if encodedSv != clean(expectedSha1) {
 		t.Errorf("mismatch")
 	}
 }
 
-//func TestSignatureVerification(t *testing.T) {
-//	sig, err := CreateSignature("data to be signed", []byte(testPrivateKey))
-//	if err != nil {
-//		t.Errorf("failed :: %v", err)
-//	}
-//
-//	encodedSv := base64.StdEncoding.EncodeToString([]byte(sig))
-//	fmt.Printf("%v", encodedSv)
-//
-//	cer, err := ioutil.ReadFile(testPrivateKey)
-//	if err != nil {
-//		t.Errorf("failed :: %v", err)
-//	}
-//	block, _ := pem.Decode(cer)
-//	if block == nil {
-//		t.Errorf("invalid certificate no PEM data found")
-//	}
-//	if block.Type != "CERTIFICATE" {
-//		t.Errorf("invalid certificate")
-//	}
-//	c, err := x509.ParseCertificate(block.Bytes)
-//	if err != nil {
-//		t.Error(err)
-//	}
-//	key, ok := c.PublicKey.(*rsa.PublicKey)
-//	if !ok {
-//		t.Errorf("certificate's public key is not RSA")
-//	}
-//	err = VerifySignature(key, []byte("data to be signed"), []byte(sig))
-//	if err != nil {
-//		t.Errorf("failed :: %v", err)
-//	}
-//}
+func TestSignatureVerification(t *testing.T) {
+	hash := crypto.SHA1
+	key, err := ioutil.ReadFile("key.pem")
+	if err != nil {
+		t.Errorf("failed :: %v", err)
+	}
+	sig, err := CreateSignature(messageToSign, key, hash)
+	if err != nil {
+		t.Errorf("failed :: %v", err)
+	}
+
+	encodedSv := base64.StdEncoding.EncodeToString([]byte(sig))
+	fmt.Printf("%v \n", encodedSv)
+
+	cer, err := ioutil.ReadFile("certificate.pem")
+	if err != nil {
+		t.Errorf("failed :: %v", err)
+	}
+	block, _ := pem.Decode(cer)
+	if block == nil {
+		t.Errorf("invalid certificate no PEM data found")
+	}
+	if block.Type != "CERTIFICATE" {
+		t.Errorf("invalid certificate")
+	}
+	c, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		t.Error(err)
+	}
+	publicKey, ok := c.PublicKey.(*rsa.PublicKey)
+	if !ok {
+		t.Errorf("certificate's public key is not RSA")
+	}
+	err = VerifySignature(publicKey, []byte("data to be signed"), []byte(sig), hash)
+	if err != nil {
+		t.Errorf("failed :: %v", err)
+	}
+}
